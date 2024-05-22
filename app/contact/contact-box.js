e400ea8be0aaa23bc507720cd6812ce589f3cec1
gangmin2 <min0115b@gmail.com>
2024-05-23 06:00:08 +0900

    스타일 수정

--- app/contact/contact-box.js
+++ app/contact/contact-box.js
@@ -1,7 +1,7 @@
 export default function ContactBox({ icon, txt, link }) {
     return (
         <a href={link} target="_blank">
-            <div className="hover:bg-slate-700" style={{ padding: '16px', display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: '16px', }}>
+            <div className="hover:bg-slate-300 dark:hover:bg-slate-700" style={{ padding: '16px', display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: '16px', }}>
                 <img src={icon} style={{ width: '70%', marginBottom: '32px', }} />
                 <p>{txt}</p>
             </div>
